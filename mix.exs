8a84fc745d74f9385a38f4a1d554263c1951a027
Allan MacGregor <amacgregor@allanmacgregor.com>
2021-11-07 18:47:12 -0500

    Add check command

--- mix.exs
+++ mix.exs
@@ -71,6 +71,7 @@ defmodule Faulkner.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+      check: ["format --check-formatted"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
