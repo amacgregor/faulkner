ad7520318a8873c3230c6cecfcc078699d4cd945
Allan MacGregor <amacgregor@allanmacgregor.com>
2021-11-11 08:21:46 -0500

    Modal updates

--- lib/faulkner_web/live/modal_component.ex
+++ lib/faulkner_web/live/modal_component.ex
@@ -6,18 +6,30 @@ defmodule FaulknerWeb.ModalComponent do
     ~H"""
     <div
       id={@id}
-      class="phx-modal"
       phx-capture-click="close"
       phx-window-keydown="close"
       phx-key="escape"
       phx-target={@myself}
-      phx-page-loading>
+      phx-page-loading
+      class="fixed z-10 inset-0 overflow-y-auto"
+      aria-labelledby="modal-title"
+      role="dialog" aria-modal="true">
+      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
-      <div class="phx-modal-content">
-        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
-        <%= live_component @component, @opts %>
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+        <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-4xl sm:w-full sm:p-6">
+          <div class="phx-modal-content">
+            <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
+            <%= live_component @component, @opts %>
+          </div>
+        </div>
       </div>
     </div>
+
     """
   end
 
